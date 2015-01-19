62c85464a672d3a37358f4500196ae4dc1b7cf48
Ben Eirich <beirich@gmail.com>
2015-01-18 23:25:02 -0500

    expand on N9Context

--- n9.core/N9Context.cs
+++ n9.core/N9Context.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace n9.core
 {
@@ -10,11 +7,48 @@ namespace n9.core
     {
         // TODO N9Context
         // list of source files
-        // Architecture [32bit or 64bit --- direct impact, size of ptr?]
         // intel, arm?
         // debug, release
         // opt-levels, compiler flags
-        // debug symbols
         // generate EXE, static library, shared library
+
+        // ==============================================================================
+
+        public ArchBits ArchBits = ArchBits.Arch32Bit;
+        public CompilationTarget CompilationTarget = CompilationTarget.Executable;
+        public CCompilerBackend Backend = CCompilerBackend.TCC;
+        public OptimizationLevel OptimizationLevel = OptimizationLevel.None;
+        public bool GenerateDebugSymbols = true;
+
+        public List<String> VersionTags = new List<string>();
+
+        // ==============================================================================
+
+        public N9Context InitTags()
+        {
+            if (GenerateDebugSymbols)
+                VersionTags.Add("debug");
+
+            switch (ArchBits)
+            {
+                case ArchBits.Arch32Bit: VersionTags.Add("x86"); VersionTags.Add("arch32bit"); break;
+                case ArchBits.Arch64Bit: VersionTags.Add("x64"); VersionTags.Add("arch64bit"); break;
+            }
+                VersionTags.Add("debug");
+
+            return this;
+        }
+
+        public N9Context DefaultReleaseSettings()
+        {
+            OptimizationLevel = OptimizationLevel.Max;
+            GenerateDebugSymbols = true;
+            return this;
+        }
     }
+
+    public enum ArchBits { Arch32Bit, Arch64Bit }
+    public enum CompilationTarget { Executable, StaticLibrary, DynamicLibrary }
+    public enum CCompilerBackend { TCC }
+    public enum OptimizationLevel { None, Some, Max }
 }
